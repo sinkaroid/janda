05527aa5840e5d0bf51fe5addd6f6bc7e9d9e057
sinkaroid <bibsindra@gmail.com>
2022-04-03 14:56:01 +0700

    build: bump version

--- janda/__init__.py
+++ janda/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "3.0.8"
+__version__ = "3.0.11"
 from janda.pururin import Pururin
 from janda.nhentai import Nhentai
 from janda.hentaifox import Hentaifox
 from janda.hentai2read import Hentai2read
 from janda.simply_hentai import SimplyHentai
 from janda.qhentai import Qhentai
+from janda.asmhentai import Asmhentai
 from janda.utils import *
