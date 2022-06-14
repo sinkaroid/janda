76c151c0755928c0ee7809ecddbf84964cb82ff5
sinkaroid <bibsindra@gmail.com>
2022-06-14 07:16:09 +0700

    chore(release): 3.0.14-dev

--- janda/__init__.py
+++ janda/__init__.py
@@ -1,9 +1,8 @@
-__version__ = "3.0.13"
+__version__ = "3.0.14"
 from janda.pururin import Pururin
 from janda.nhentai import Nhentai
 from janda.hentaifox import Hentaifox
 from janda.hentai2read import Hentai2read
 from janda.simply_hentai import SimplyHentai
-from janda.qhentai import Qhentai
 from janda.asmhentai import Asmhentai
 from janda.utils.parser import resolve
