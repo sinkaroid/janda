3a69fbcfb90aa1e5e47ab5ca7dd207d8b4de71cc
sinkaroid <bibsindra@gmail.com>
2025-06-12 03:50:18 +0700

    ci: forgor to unittest build tho

--- test/test_build.py
+++ test/test_build.py
@@ -1,2 +1,12 @@
+import unittest
 import janda
-print(janda.__version__)
+
+class TestVersion(unittest.TestCase):
+
+    def test_version(self):
+        print(janda.__version__)
+        self.assertIsInstance(janda.__version__, str)
+        self.assertRegex(janda.__version__, r"^\d+\.\d+\.\d+$")
+
+if __name__ == "__main__":
+    unittest.main()
