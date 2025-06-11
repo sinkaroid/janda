c97e14f6a35bb9b3ffab2473de1b7e1720ac288a
sinkaroid <bibsindra@gmail.com>
2025-06-12 03:20:45 +0700

    ci: test api mock

--- test/test_api.py
+++ test/test_api.py
@@ -1,19 +1,25 @@
 import aiohttp
-import asyncio
+import unittest
+
 from janda.utils.client import list_api
 
-async def test_api():
-    async with aiohttp.ClientSession() as session:
-        for api in list_api():
-            if "nhen" in api:
-                get_random = api + "/get?book=577774"
-            elif "hentai2read" in api:
-                get_random = api + "/search?key=futanari"
-            elif "simply-hentai" in api:
-                get_random = api + "/get?book=fate-grand-order/fgo-sanbunkatsuhou/all-pages"
-            else:
-                get_random = api + "/random"
-            async with session.get(get_random) as resp:
-                print(get_random, resp.status)
+class TestAPI(unittest.IsolatedAsyncioTestCase):
 
-asyncio.run(test_api())
+    async def test_api_endpoints(self):
+        async with aiohttp.ClientSession() as session:
+            for api in list_api():
+                if "nhen" in api:
+                    get_random = api + "/get?book=577774"
+                elif "hentai2read" in api:
+                    get_random = api + "/search?key=futanari"
+                elif "simply-hentai" in api:
+                    get_random = api + "/get?book=fate-grand-order/fgo-sanbunkatsuhou/all-pages"
+                else:
+                    get_random = api + "/random"
+
+                async with session.get(get_random) as resp:
+                    print(get_random, resp.status)
+                    self.assertEqual(resp.status, 200, msg=f"{get_random} returned {resp.status}")
+
+if __name__ == "__main__":
+    unittest.main()
